27faa4df219ad968a47e19f46930faf18a39cb06
Jimmy García <jimmyegc@gmail.com>
2024-07-01 19:56:04 -0600

    fix 07012024

--- src/components/DropArea.tsx
+++ src/components/DropArea.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import './DropArea.css'
+import './DropArea.scss'
 export const DropArea = ({ onDrop }) => {
   const [showDrop, setShowDrop] = useState(false)
   return (<>
@@ -11,8 +11,7 @@ export const DropArea = ({ onDrop }) => {
         onDrop();
       }}
       onDragOver={(e) => e.preventDefault()}
-      className={`bg-blue-400 m-1 p-4 border-4 border-dotted 
-        ${showDrop ? "drop_area" : "hide_drop"}`}
+      className={`${showDrop ? "drop_area" : "hide_drop"}`}
     >DropArea</div>
   </>)
 }
